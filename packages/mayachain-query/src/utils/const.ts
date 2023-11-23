a237b569dfb10470d07dd54412ec84bba7d4ffff
GiMa-Maya <128942712+GiMa-Maya@users.noreply.github.com>
2023-11-23 16:07:10 +0100

    Create const.ts

---
+++ packages/mayachain-query/src/utils/const.ts
@@ -0,0 +1,19 @@
+import { Asset, assetFromStringEx, assetToString } from '@xchainjs/xchain-util'
+
+export const AssetBTC = assetFromStringEx('BTC.BTC')
+export const AssetDASH = assetFromStringEx('DASH.DASH')
+export const AssetETH = assetFromStringEx('ETH.ETH')
+export const AssetKUJI = assetFromStringEx('KUJI.KUJI')
+export const AssetMAYA = assetFromStringEx('MAYA.CACAO')
+export const AssetRUNE = assetFromStringEx('THOR.RUNE')
+export const assetUSDC = assetFromStringEx('ETH.USDC-0XA0B86991C6218B36C1D19D4A2E9EB0CE3606EB48')
+export const assetUSDT = assetFromStringEx('ETH.USDT-0xdAC17F958D2ee523a2206206994597C13D831ec7')
+export const assetWSTETH = assetFromStringEx('ETH.WSTETH-0X7F39C581F595B53C5CB19BD0B3F8DA6C935E2CA0')
+export const assetUSK = assetFromStringEx('KUJI.USK')
+
+export const BTCChain = 'BTC'
+export const DASHChain = 'DASH'
+export const KUJIChain = 'KUJI'
+export const GAIAChain = 'GAIA'
+export const MAYAChain = 'MAYA'
+export const THORChain = 'THOR'
